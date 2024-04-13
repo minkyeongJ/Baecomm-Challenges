e3356cf5a0bceb264518b585facce9fef6d077ca
minkyeongJ <jomk2221@naver.com>
2024-04-13 20:21:06 +0900

    Feat: 상품 호출 api 구현

---
+++ src/apis/fetchSelectProductList.ts
@@ -0,0 +1,26 @@
+import { API_URL } from "../helper/constants/apiUrl";
+import { ProductList } from "../types/products";
+
+interface Props {
+  query?: string;
+  limit?: number;
+  skip?: number;
+}
+
+const fetchSelectProductList = async ({
+  query = "",
+  limit = 10,
+  skip = 0,
+}: Props) => {
+  try {
+    const response = await fetch(
+      `${API_URL}/products?search?q=${query}&limit=${limit}&skip=${skip}`
+    );
+    const data = await response.json();
+    return data as ProductList;
+  } catch (error) {
+    console.error("API 데이터를 불러오는 중 오류가 발생했습니다:", error);
+  }
+};
+
+export default fetchSelectProductList;
